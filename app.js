9fcef3fc9d62b4b159ac94a446dca66ed51f7a6c
luisariasg <luisarias.1@outlook.com>
2025-02-12 21:56:13 -0600

    ACtualización

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
+
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+
+
+
+function agregarAmigo(){
+    
+    let imput = document.getElementById("amigo").value;
+
+    if(imput.length === 0){
+
+    alert("Debes ingresar el nombre de tu amigo");
+
+    }else{
+        
+        let nombre = document.getElementById("amigo");
+    amigos.push(nombre);
+    console.log(amigos);
+    document.getElementById("amigo").value = "";
+
+    }
+
+}
