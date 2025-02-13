56f39afdb05bafc9bee872a5b2503e054db49078
luisariasg <luisarias.1@outlook.com>
2025-02-12 23:13:26 -0600

    actualizacion

--- app.js
+++ app.js
@@ -7,29 +7,28 @@ let amigos = [];
 
 
 // Esta funcion guarda en un array los nombres ingresados, validando si el espacio esta en blanco antes de ejecutar
-function agregarAmigo(){
-    
+function agregarAmigo() {
+
     let imput = document.getElementById("amigo").value;
 
-    if(imput.length === 0){
+    if (imput.length === 0) {
 
-    alert("Debes ingresar el nombre de tu amigo");
-
-    }else{
-    
-    listaAmigos.innerText="";   
-    let nombre = document.getElementById("amigo").value;   
-    amigos.push(nombre);
-    document.getElementById("amigo").value = "";
-
+     alert("Debes ingresar el nombre de tu amigo");
+     //se limpia ul para evitar doble impresíon en pantalla
+     listaAmigos.innerText = "";
+    } else {
+        listaAmigos.innerText = "";
+        let nombre = document.getElementById("amigo").value;
+        amigos.push(nombre);
+        document.getElementById("amigo").value = "";
     }
-    
+    // se encarga de imprimir la lista en UL cada vez que se agrera un nombre
     imprimirListaAmigos();
 
 }
 
-function imprimirListaAmigos(){
-    
+function imprimirListaAmigos() {
+    // recorre el array para imprimirlo el ul , creando nuevas li para cada nombre
     const listaAmigos = document.getElementById('listaAmigos');
 
     amigos.forEach(amigo => {
